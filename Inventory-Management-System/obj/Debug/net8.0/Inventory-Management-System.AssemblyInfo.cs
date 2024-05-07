d15b80d705c32f2a306694b7f002fd751ab7088c
Abdullah-786 <ar1627276@gmail.com>
2024-05-08 02:07:01 +0530

    added commit

--- Inventory-Management-System/obj/Debug/net8.0/Inventory-Management-System.AssemblyInfo.cs
+++ Inventory-Management-System/obj/Debug/net8.0/Inventory-Management-System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory-Management-System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94be5f4b59253a910aecca45be14e8643c435c36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory-Management-System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory-Management-System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
